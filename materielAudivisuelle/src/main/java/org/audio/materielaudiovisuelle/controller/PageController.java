5d110454e67b4e800370c8e39dade17b083e4546
soukaina-bounzaha <nadinehouari1@gmail.com>
2020-06-02 23:52:37 +0200

    successfully loaded bootstrap ressources

---
+++ materielAudivisuelle/src/main/java/org/audio/materielaudiovisuelle/controller/PageController.java
@@ -0,0 +1,16 @@
+package org.audio.materielaudiovisuelle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+ public class PageController {
+         @RequestMapping(value = {"/","/home","/index"})
+            public ModelAndView index() {
+        	 ModelAndView  mv = new ModelAndView("page");
+        	 mv.addObject("hey","soukaina was here");
+        	 return mv ;
+         }
+         
+}
